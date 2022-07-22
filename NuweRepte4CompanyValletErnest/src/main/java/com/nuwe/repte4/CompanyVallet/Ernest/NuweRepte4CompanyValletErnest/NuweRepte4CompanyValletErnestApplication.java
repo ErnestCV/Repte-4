04d2ecb88decdf4fc18fd116746139063ebc1156
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-07-22 15:03:23 +0200

    Add files via upload

---
+++ NuweRepte4CompanyValletErnest/src/main/java/com/nuwe/repte4/CompanyVallet/Ernest/NuweRepte4CompanyValletErnest/NuweRepte4CompanyValletErnestApplication.java
@@ -0,0 +1,13 @@
+package com.nuwe.repte4.CompanyVallet.Ernest.NuweRepte4CompanyValletErnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuweRepte4CompanyValletErnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuweRepte4CompanyValletErnestApplication.class, args);
+	}
+
+}
---
+++ NuweRepte4CompanyValletErnest/src/test/java/com/nuwe/repte4/CompanyVallet/Ernest/NuweRepte4CompanyValletErnest/NuweRepte4CompanyValletErnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuwe.repte4.CompanyVallet.Ernest.NuweRepte4CompanyValletErnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuweRepte4CompanyValletErnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
